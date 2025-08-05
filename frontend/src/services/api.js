6049a580e8b1feb49f3283ce1f5f10d654e60eca
Faseeh844 <billalahamed84444@gmail.com>
2025-08-05 23:59:17 +0530

    .

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api', // Your Django API base URL
+    baseURL: 'https://my-blog-backend.onrender.com/api', // <-- Your live backend URL
     headers: {
         'Content-Type': 'application/json',
     },
